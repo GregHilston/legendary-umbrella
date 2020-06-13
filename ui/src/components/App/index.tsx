713ca1fe91bad2653d12e0cb2430bf0804e07742
David Harrigan <davidharrigan@users.noreply.github.com>
2020-06-12 23:20:39 -0400

    shitty light control

--- ui/src/components/App/index.tsx
+++ ui/src/components/App/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './app.module.scss';
 
-import Fun from '../Fun'
+import Fun from '../Fun';
+import LightControl from '../LightControl';
 
 function App() {
   return (
     <div className='App'>
       <h1 className='meme-spacing center'>LEGENDARY UMBRELLA</h1>
       <Fun/>
+      <LightControl/>
     </div>
   );
 }
