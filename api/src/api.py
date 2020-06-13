1e411e04e938f34c3555f49072cc586ac05c15bf
Jesse Mailhot <pogopunkxiii@gmail.com>
2020-06-12 23:46:20 -0400

    very important features

--- api/src/api.py
+++ api/src/api.py
@@ -28,3 +28,17 @@ def scenes():
 def lights():
     response = requests.get(url_base + '/lights')
     return response
+
+@api.route('/lights/<id>/state')
+def set_light():
+    response = request.put(url_base + '/lights/' + id + '/state')
+    return response
+
+@api.route('/rainbow/<int: revolutions>')
+def rainbow():
+    for(i in range(0, revolutions)):
+        for(color in range(0, 65535):
+            color = 0
+
+            response = request.put(url_base + 'lights/1/state', json={"hue":color})
+    return response
