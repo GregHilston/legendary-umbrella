f2f686e7e2128b29471331dd416aa7cea1aea8f3
Jesse Mailhot <pogopunkxiii@gmail.com>
2020-06-12 23:00:19 -0400

    I wrote some shit, don't look at it

--- api/src/api.py
+++ api/src/api.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from dotenv import load_dotenv
+import requests
 
+load_dotenv()
 
 api = Flask(__name__)
 
+PILLIPS_TOKEN = os.getenv('PHILLIPS_TOKEN')
+
+url_base = 'api/' + PHILLIPS_TOKEN
 
 @api.route("/")
 def hello_world():
     return "Hello, World!"
+
+@api.route("/groups")
+def groups():
+    response = requests.get(url_base + '/groups')
+    return response
+
+@api.route("/scenes")
+def scenes():
+    response = requests.get(url_base + '/scenes')
+    return response
+
+@api.route('/lights')
+def lights():
+    response = requests.get(url_base + '/lights')
+    return response
